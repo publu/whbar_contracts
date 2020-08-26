a9b73456c5189e510bab3ee6725a3deaafe15cf3
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2020-08-26 17:03:55 -0500

    contracts/whbar_erc20: Change include paths to point at local files

--- contracts/whbar_erc20.sol
+++ contracts/whbar_erc20.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.1/contracts/token/ERC20/ERC20Burnable.sol";
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.1/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract whbar is ERC20Burnable, ERC20Detailed {
     address validator;

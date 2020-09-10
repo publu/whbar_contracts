92d04a640a209cf0ed510edd838c1b262891ffe0
Pablo S Peillard <Pjs130130@utdallas.edu>
2020-09-10 16:04:06 -0500

    pause and getters

--- contracts/hbar_filter.sol
+++ contracts/hbar_filter.sol
@@ -3,12 +3,30 @@ pragma solidity =0.5.0;
 contract hbar_filter {
     address payable admin;
     uint min = 0;
-   
+    bool pause = false;
     constructor() public {
         admin = msg.sender;
     }
+    function getPause() view public returns(bool){
+      return pause;
+    }
+    function getMin() view public returns(uint){
+      return min;
+    }
+    function getAdmin() view public returns(address){
+      return admin;
+    }
+    function togglePause() public {
+        require(admin==msg.sender);
+        if(pause){
+          pause=false;
+        }else{
+          pause=true;
+        }
+    }
     function changeMin(uint m) public {
         require(admin == msg.sender);
+        require(m >= 0);
         min = m;
     }
     function changeAdmin(address payable newOwner) public {
@@ -19,5 +37,5 @@ contract hbar_filter {
       require(msg.value >= min);
       admin.transfer(msg.value);
     }
-    // withdraw is done via multi-sig wallet
+    // withdraw is done via multi-sig wallet in a TEE
 }
